8115582f940f96ff48d5d5be2aafce521a2f477e
Aman Tyagi <taman7786@gmail.com>
2026-08-16 00:05:56 +0530

    feat: add initial frontend structure with authentication and routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,16 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
 import {connectDB} from './lib/db.js';
 import dotenv from 'dotenv';
+import cors from "cors"
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 const PORT = process.env.PORT || 3000;
 
