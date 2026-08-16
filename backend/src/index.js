894b3002861da2dfc23ac03d1cadab237e725887
Aman Tyagi <taman7786@gmail.com>
2026-08-16 23:47:52 +0530

    update project structure and configuration for frontend and backend integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ import {connectDB} from './lib/db.js';
 import cors from "cors"
 import { app,server } from "./lib/socket.js"
 
+import path from "path";
+
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 app.use(cors({
@@ -16,9 +18,17 @@ app.use(cors({
 }))
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
